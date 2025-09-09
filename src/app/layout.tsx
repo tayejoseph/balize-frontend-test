91953e210989e5029b1976f53084fb21e5f40a25
Taye Joseph <tayejoseph6@gmail.com>
2025-09-09 23:53:47 +0100

    Build and cleanup: Remove unused components and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import "./globals.css";
 import type { PropsWithChildren } from "react";
-// import { QueryProvider } from "../components";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body>
         <h1>Hello Testing Ora</h1>
-        {/* <QueryProvider>{children}</QueryProvider> */}
+        {children}
       </body>
     </html>
   );
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as QueryProvider } from "./QueryProvider";
-export { default as ScrollToTopButton } from "./ScrollToTopButton";
-export { default as SkeletonLoader } from "./SkeletonLoader";
-export { default as PokemonRow } from "./PokemonRow";
-export { default as PokemonCardRow } from "./PokemonCardRow";
